683daa2e16e4ff6dc2ae1b9a575c8ed75846533b
Meli <m507@live.com.mx>
2015-03-05 23:42:48 -0600

    cambio documentos

--- js/Controllers/documentosController.js
+++ js/Controllers/documentosController.js
@@ -11,13 +11,54 @@ app.controller("documentosController", function ($scope, $http, dataResource,$ro
         $scope.documentosmostrar = data;
 
             window.open($scope.documentosmostrar[index].documento)
-            event.preventDefault();
             console.log(data)
         });
 
     };
+    var timeoutHandle = window.setTimeout(function(){
+        $("#message1").addClass('hide');
+
+    });
+
+    $(".errorArchivo").addClass('hide');
+
+    
+    $scope.guardarDocumento = function(){
+        var descripcion = $scope.documento.descripcion,
+        titulo = $scope.documento.titulo,
+        grupo = $scope.documento.grupo,
+        documentoNuevo = new Object(),
+        fechaDocumento = new Date();
+
+        documentoNuevo.titulo = titulo;
+        documentoNuevo.grupo = grupo;
+        documentoNuevo.descripcion = descripcion;
+        documentoNuevo.fecha = fechaDocumento.getDate() + '/' + fechaDocumento.getMonth() + '/' + fechaDocumento.getFullYear();
+
+
+        $scope.documentosTotales.push(documentoNuevo);
+        
+        $('#myModal').modal('hide');
+        window.clearTimeout(timeoutHandle);
+
+            $("#message1").removeClass('hide');
+            timeoutHandle = window.setTimeout(function(){
+                $("#message1").addClass('hide');
+            }, 2000);
+
+        console.log(documentoNuevo);
+
+
+        $scope.documento.titulo = " ";
+        $scope.documento.descripcion = " ";
+        $scope.documento.tema = " ";
+    };
+
+    $scope.ver = 1;
 
 });
+
+
 app.factory("dataResource", function ($resource) {
     return $resource("../data/documento.json", 
         { get: { method: "GET", isArray: true }
