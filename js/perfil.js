d77f80d4d26e5d3e7884f22123eff5f2ac35a488
Daniel J. Campos Arce <daniel.camposarce@gmail.com>
2015-03-06 12:44:22 -0600

    Merge entre lo que Daniel le hace funcionar, y lo que guille funciona.
    Esta mezcla fue minima, más que todo eliminar los modulos de "Daniel" y
    agregarselos a los de guille.

--- js/perfil.js
+++ js/perfil.js
@@ -1,21 +1,4 @@
-var app = angular.module('CoreApp', ['ngRoute', 'ngAnimate']);
-
-//Configuración de enrutamiento
-app.config(['$routeProvider', '$locationProvider',
-	function ($routeProvider, $locationProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: 'pages/home.html',
-			})
-			.when('/perfil', {
-				templateUrl: 'pages/_perfil.html',
-				controller: 'PerfilCtrl'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-		$locationProvider.html5Mode(true);
-}]);
+var app = angular.module('module', ['ngAnimate']);
 //Directivas
 app.directive('slideshow', [
 
